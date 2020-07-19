917c50dbc3cf95e9c5f025d3a5141f35abf411b0
Kirill Nagaitsev <knagaitsev1@gmail.com>
2020-07-19 15:44:13 -0500

    delay between sends

--- index.js
+++ index.js
@@ -3,12 +3,22 @@ const SerialPort = require('serialport');
 
 const serialport = new SerialPort('/dev/ttyS1');
 
+const delay = (time) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, time);
+  });
+};
+
 const init = () => {
   const ws = new WebSocket('ws://192.168.1.2:1880/ws');
 
-  ws.on('message', (data) => {
-    serialport.write(data);
-    console.log(data);
+  ws.on('message', async (data) => {
+    for (let i = 0; i < data.length; i++) {
+      serialport.write(data.charAt(i));
+      console.log(data.charCodeAt(i));
+
+      await delay(100);
+    }
   });
 
   ws.on('close', () => {
